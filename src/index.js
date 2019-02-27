8f7e5458559a0a3806d48d480459b361028711e9
Maksim Antonenko <Maksim_Antonenko@epam.com>
2019-02-27 16:16:51 +0400

    tasked solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const sudocu = require("./sudoku");
+
 module.exports = function solveSudoku(matrix) {
   // your solution
-}
+  let result = [];
+  let solved = sudocu(matrix);
+  for (let i = 0; i < 9; i++)
+  {
+    result[i] = solved[i].map(ar => ar[0]);
+  }
+  return result;
+} 
+
